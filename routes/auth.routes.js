b3d1a1c3ed41617ce593de7a41a6e523f214d653
Cindy <cindytvn+git@gmail.com>
2021-02-09 17:36:37 +0100

    singup with encrypting

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,35 @@
 const router = require("express").Router();
+const bcrypt = require('bcryptjs');
+const UserModel = require('../models/User.model') 
 
-// GET || Add a GET route for /signup
+
+// GET || request for /signup
 // shows signup page when user visit route
 router.get('/signup', (req, res, next) => {
     res.render('auth/signup.hbs')
   })
 
-// GET || Add GET route for /singin  
+// GET || request for /singin  
 router.get('/signin', (req, res, next) => {
   res.render('auth/signin.hbs')
 }) 
+
+// POST || request for /singup
+router.post("/signup", (req, res, next) => {
+  // grab data from input form
+  const {name, email, password} = req.body
+  // console.log(req.body) // check ||  firsttime for empy object
+
+  let salt = bcrypt.genSaltSync(10);
+  let hash = bcrypt.hashSync(password, salt);
+  UserModel.create({name, email, password: hash})
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch((err) => {
+      next(err)
+    })
+});
 
 
 // BOTTOM plz: 
