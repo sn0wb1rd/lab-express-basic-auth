eb08f761184b29681373d0bb07f9759ce4db4142
Cindy <cindytvn+git@gmail.com>
2021-02-09 17:21:51 +0100

    nav bar added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,10 +9,10 @@ router.get('/signup', (req, res, next) => {
 // GET || Add GET route for /singin  
 router.get('/signin', (req, res, next) => {
   res.render('auth/signin.hbs')
-})
+}) 
 
 
 // BOTTOM plz: 
 // this links router in middleware 
 // at the bottom of app.js where the other routes are defines
-module.exports = router;
+module.exports = router;  
