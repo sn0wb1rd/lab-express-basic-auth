ec614e0f06ee3d9d5c53a4ed6f13a977a5352dff
Cindy <cindytvn+git@gmail.com>
2021-02-10 07:00:29 +0100

    signin with both async as sync bcrypt method

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,18 +18,81 @@ router.get('/signin', (req, res, next) => {
 router.post("/signup", (req, res, next) => {
   // grab data from input form
   const {name, email, password} = req.body
-  // console.log(req.body) // check ||  firsttime for empy object
+  console.log(req.body) // check ||  firsttime for empy object
 
+  // using ONE method with bcrypt..
   let salt = bcrypt.genSaltSync(10);
   let hash = bcrypt.hashSync(password, salt);
   UserModel.create({name, email, password: hash})
     .then(() => {
-      res.redirect('/')
+      res.redirect('/') 
     })
     .catch((err) => {
       next(err)
     })
 });
+
+// POST || request for singin
+router.post("/signin", (req, res, next) => {
+  const {email, password} = req.body
+  // console.log(req.body) // check
+
+  // implement the same set of validations as you did in signup as well
+  // NOTE: We have used the Async method here. Its just to show how it works
+  UserModel.findOne({email: email})
+      .then((result) => {
+          //if user exists
+          if (result) {
+              //Using SYNC bcrypt method -----  
+              //check if pw match with the db
+              let isMatching = bcrypt.compareSync(password, result.password)
+              if (isMatching) {
+                  console.log('check2')
+                  // signin succesfull
+                  res.redirect('/profile')
+              }
+              else {
+                  console.log('check3')
+                  // when passwords don't match
+                  res.render('auth/signin.hbs', {msg: 'Wrong password'})
+              }
+              // END SYNC method ------------
+
+            //     //Using SYNC bcrypt method -----  
+            //     bcrypt.compare(password, result.password)
+            //         .then((isMatching) => {
+            //             if (isMatching) {
+            //                 // when the user successfully signs up
+            //                   res.redirect('/profile')
+            //             }
+            //             else {
+            //                 // when passwords don't match
+            //                 res.render('auth/signin.hbs', {msg: 'Wrong password'})
+            //             }
+            //         })
+            //      //END ASYNC METHOD -------
+            // when email does not exist
+
+            }
+            else {
+                res.render('auth/signin.hbs', {msg: 'Email does not exist'})
+            }
+      })
+      .catch((err) => {
+          next(err)
+      }) 
+});
+
+
+
+
+
+// GET || request for /profile
+router.get('/profile', (req, res, next) => {
+  res.render('profile.hbs')
+})
+
+
 
 
 // BOTTOM plz: 
