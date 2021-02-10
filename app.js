b8ad5d784fb47f137d0f484160db69c978c54437
Cindy <cindytvn+git@gmail.com>
2021-02-10 07:28:01 +0100

    setup mongoose connection in appjs

--- app.js
+++ app.js
@@ -24,6 +24,25 @@ const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowe
 
 app.locals.title = `${capitalized(projectName)}- Generated with IronGenerator`;
 
+// set up connect Mongo
+const session = require('express-session');
+const MongoStore = require('connect-mongo'(session))
+const mongoose = require('mongoose');
+
+app.use(session({
+    secret: 'NotMyAge',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000*60*60*24 // in miliseconds. Exp in 1 day
+    },
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 60*60*24 // is in seconds. Exp in 1 day
+    })
+}));
+
+
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
